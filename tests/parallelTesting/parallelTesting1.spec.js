b6624b24649ea93f253bb9dc998b007aeb364008
Mazidul <md.mazidulhasan1@gmail.com>
2024-08-11 01:23:46 +0600

    Parallel workers

---
+++ tests/parallelTesting/parallelTesting1.spec.js
@@ -0,0 +1,21 @@
+// we can make paralle testing by defining it in confing.js
+// file. Or we can define it on each file level also like this::
+
+
+const { test, expect } = require("allure-playwright")
+
+test.describe.configure({
+    mode: "serial"
+})
+
+test('Playwright parallel mode 1.1', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    await page.waitForTimeout(2000);
+});
+
+test('Playwright parallel mode 1.2', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    await page.waitForTimeout(2000);
+});
